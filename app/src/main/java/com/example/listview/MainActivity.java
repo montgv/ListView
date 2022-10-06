38588de8dfafb23d3418d4e2950e7eb0ac499133
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-06 09:33:24 +0200

    ListView Personalizado terminado

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.example.listview.databinding.ActivityMainBinding;
@@ -16,7 +15,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ActivityMainBinding binding;
-    private List<String> miLista;
+    private List<Opcion> miLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +24,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(binding.getRoot());
 
         miLista = new ArrayList<>();
-        miLista.add("Lista 1");
-        miLista.add("Lista 2");
-        miLista.add("Lista 3");
-        miLista.add("Lista 4");
-        miLista.add("Lista 5");
+        miLista.add(new Opcion("Lista 1"));
+        miLista.add(new Opcion("Lista 2"));
+        miLista.add(new Opcion("Lista 3"));
+        miLista.add(new Opcion("Lista 4"));
+        miLista.add(new Opcion("Lista 5"));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, miLista);
-
-        binding.listView.setAdapter(adapter);
+        MiAdaptador miAdaptador = new MiAdaptador(this, R.layout.opciones_item, miLista);
+        binding.listView.setAdapter(miAdaptador);
 
         binding.listView.setOnItemClickListener(this);
     }
--- app/src/main/java/com/example/listview/MiAdaptador.java
+++ app/src/main/java/com/example/listview/MiAdaptador.java
@@ -1,15 +1,50 @@
 package com.example.listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
 public class MiAdaptador extends ArrayAdapter {
 
+    private Context ctx;
+    private int layoutTemplate;
+    private List<Opcion> opcionLista;
+
     public MiAdaptador(@NonNull Context context, int resource, @NonNull List objects) {
         super(context, resource, objects);
+
+        this.ctx = context;
+        this.layoutTemplate = resource;
+        this.opcionLista = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = LayoutInflater.from(ctx).inflate(layoutTemplate, parent, false);
+
+        Opcion elementoActual = (Opcion) opcionLista.get(position);
+
+        TextView textViewOpciones = (TextView) v.findViewById(R.id.textViewItem);
+        ImageView imagenViewOpciones = (ImageView) v.findViewById(R.id.imageViewItem);
+
+        textViewOpciones.setText(elementoActual.getOpcion());
+
+        if (elementoActual.getOpcion() == "Lista 1") {
+            imagenViewOpciones.setImageResource(R.drawable.ic_uno);
+        } else {
+            imagenViewOpciones.setImageResource(R.drawable.ic_dos);
+        }
+
+        return v;
     }
 }
